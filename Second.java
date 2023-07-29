e597d9f6dc7e54ae9a16e55968721757a5dcc309
VIRENDRA TIWARI <vtiwari709@gmail.com>
2023-07-29 18:23:10 +0530

    Seekh gaya

---
+++ Second.java
@@ -0,0 +1,9 @@
+public class Second {
+    public static void main(String[] args) {
+        System.out.println("Kaisi lagi ");
+        System.out.println("Darr gaya na");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
